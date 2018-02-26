039d3a0ee4e952839fbd71e41e844b295ae88b83
Acedia55 <36156893+Acedia55@users.noreply.github.com>
2018-02-26 12:51:31 +0000

    Coding

---
+++ Assets/ExampleGame/scripts/Goomba.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goomba : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+
+        Player mario = collision.collider.GetComponent<player>();
+        mario.ApplyDamage(2);
+    }
+}
--- Assets/ExampleGame/scripts/Player.cs
+++ Assets/ExampleGame/scripts/Player.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public string logText = "195 IQ Ma Dudes";
-    public float speed = 2
+    public float speed = 2;
+    public float health = 10;
+
     // Use this for initialization
     void Start () {
 
         Debug.Log(logText);
-        Rigidbody2D rigifBody = GetComponent<Rigidbody2D>();
-        
-        rigidBody.velocity = Vector2.right * speed;
+        ApplyDamage(1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Debug.Log(logText);
+
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = Vector2.right * speed;
+
+
+    }
+   public void ApplyDamage (float damageToDeal)
+    {
+        health = health - damageToDeal;
+    }
 }
