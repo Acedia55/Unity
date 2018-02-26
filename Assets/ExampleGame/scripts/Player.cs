0fa32c7927412c0ea34004385e1347188746da93
Acedia55 <36156893+Acedia55@users.noreply.github.com>
2018-02-26 11:24:58 +0000

    Example Player CAOSE

--- Assets/ExampleGame/scripts/Player.cs
+++ Assets/ExampleGame/scripts/Player.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello Ma Dudes");
+    public string logText = "195 IQ Ma Dudes";
+    public float speed = 2
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log(logText);
+        Rigidbody2D rigifBody = GetComponent<Rigidbody2D>();
+        
+        rigidBody.velocity = Vector2.right * speed;
 	}
 	
 	// Update is called once per frame
