5b3803eed9ccbf7f61175b0e2d95f0d46a7d25bf
Acedia55 <36156893+Acedia55@users.noreply.github.com>
2018-03-05 10:49:29 +0000

    Jumo Code

--- Assets/ExampleGame/scripts/Player.cs
+++ Assets/ExampleGame/scripts/Player.cs
@@ -5,7 +5,9 @@ public class Player : MonoBehaviour {
 
     public string logText = "195 IQ Ma Dudes";
     public float speed = 2;
+    public float Jumpspeed = 6;
     public float health = 10;
+
 
     // Use this for initialization
     void Start () {
@@ -22,18 +24,34 @@ public class Player : MonoBehaviour {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+
+        bool Jump = Input.GetButtonDown("Jump");
+
+        Collider2D collidor = GetComponent<Collider2D>();
+
+        LayerMask groundLayer = LayerMask.GetMask("Ground");
+
+        bool touchingGround = collidor.IsTouchingLayers(groundLayer);
+
+       // Debug.Log(horizontal);
 
         Vector2 velocity = rigidbody.velocity;
         velocity.x = horizontal * speed;
+
+        if (Jump == true && touchingGround == true)
+        {
+            velocity.y = Jumpspeed;
+        }
 
         rigidbody.velocity = velocity;
 
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Mouse Button Pressed");
+          //  Debug.Log("Mouse Button Pressed");
         }
 
+        Vector2 mousePosition = Input.mousePosition;
+       // Debug.Log("Mouse Pos is " + mousePosition);
 
     }
    public void ApplyDamage (float damageToDeal)
